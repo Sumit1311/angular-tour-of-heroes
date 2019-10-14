f3eff10a310ba618912029b9efe0b2c44ef1de58
Sumit Toshniwal <sumit@mindpathtech.com>
2019-10-14 11:32:43 +0530

    Added final part of tutorial

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -74,4 +74,15 @@ export class HeroService {
       catchError(this.handleError<Hero>('deleteHero'))
     );
   }
+
+  searchHeroes(term:string): Observable<Hero[]> {
+    if(!term.trim()) {
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`)
+    .pipe(
+      tap((heroes) => this.log(`Got search results ${heroes.length}`)),
+      catchError(this.handleError<Hero[]>('searchHeroes'))
+    )
+  }
 }
